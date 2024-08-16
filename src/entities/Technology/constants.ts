7da6c306a4d8e0706ae9922f43e51ee62555158a
Ivan Dmitrievich <quentin.gothic@gmail.com>
2024-08-16 08:38:00 +0300

    feat: add new images in stack

--- src/entities/Technology/constants.ts
+++ src/entities/Technology/constants.ts
@@ -1,34 +1,48 @@
+import bootstrap from './images/bootstrap.svg';
+import CSS from './images/css.svg';
 import cypress from './images/cypress.svg';
 import docker from './images/docker.svg';
+import figma from './images/figma.svg';
+import FSD from './images/fsd.svg';
 import git from './images/git.svg';
 import github from './images/github.svg';
 import gmail from './images/gmail.svg';
+import gulp from './images/gulp.svg';
+import HTML from './images/html.svg';
+import i18n from './images/i18n.svg';
 import javaScript from './images/javascript.svg';
 import jest from './images/jest.svg';
-import kubernetes from './images/kubernetes.svg';
-import nextJs from './images/next-js.svg';
+import mui from './images/mui.svg';
 import react from './images/react.svg';
 import redux from './images/redux.svg';
+import restAPI from './images/rest.svg';
+import SCSS from './images/scss.svg';
 import storybook from './images/storybook.svg';
-import styledComponents from './images/styled-components.svg';
 import telegram from './images/telegram.svg';
 import typeScript from './images/typescript.svg';
 import webpack from './images/webpack.svg';
 
 export const technologyImages = {
+	bootstrap,
+	CSS,
 	cypress,
 	docker,
+	figma,
+	FSD,
 	git,
 	github,
 	gmail,
+	gulp,
+	HTML,
+	i18n,
 	javaScript,
 	jest,
-	kubernetes,
-	nextJs,
+	'Material UI': mui,
 	react,
 	redux,
+	'REST API': restAPI,
+	SCSS,
 	storybook,
-	'styled-components': styledComponents,
 	telegram,
 	typeScript,
 	webpack,
